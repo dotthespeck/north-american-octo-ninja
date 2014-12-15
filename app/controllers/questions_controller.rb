0cf51a7482ea4c3b97a8ee2a3ed9545b4ef734c1
Zoe <doticus@gmail.com>
2014-12-15 17:26:19 -0500

    Form working for new submissions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,8 @@
-class QuestionsController < ActionController::Base
+class QuestionsController < ApplicationController
+
+  def current_user_id
+    1
+  end
 
   def index
     @questions = Question.all.order(created_at: :desc)
@@ -6,6 +10,25 @@ class QuestionsController < ActionController::Base
   end
 
   def show
+    id = params[:id]
+    @question = Question.find(id)
   end
 
+  def create
+    @question = Question.new(question_params)
+    @question.user_id = current_user_id
+    if @question.save
+      redirect_to @question, notice: "Question was successfully created"
+    else
+      render :new
+    end
+  end
+
+    def new
+      @question = Question.new
+    end
+
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,8 @@ User.create(name: "Cindy")
 
 Question.create(title: "Here is a question that I have and want to ask", description: "Here is a description of the question that I have. It has to be pretty long so I am saying a lot. Here we go. It is like the equivalent of two tweets, which is a lot of text", user_id: 1)
 Question.create(title: "Yet another question that really should be asked", description: "Here is a description of the question that I have. It has to be pretty long so I am saying a lot. Here we go. It is like the equivalent of two tweets, which is a lot of text", user_id: 3)
+Question.create(title: "I am stuck in Space Boston too. How do I get out? Space Boston is cold", description: "Somehow I got stuck in Space Boston and now I can't get out. I think I would rather be in Space Paris. But maybe not. Maybe Space Paris is smelly and I would be lonely", user_id: 2)
 
 Answer.create(question_id: 2, user_id: 3, answer: "Here is an answer to a question, I hope that it is helpful. Maybe it will be. Maybe it won't be. Maybe it needs to be really long. Probably. It needs to be really long. I need to have a lot to say.")
 Answer.create(question_id: 3, user_id: 1, answer: "Here is an answer to a question, I hope that it is helpful. Maybe it will be. Maybe it won't be. Maybe it needs to be really long. Probably. It needs to be really long. I need to have a lot to say.")
+Answer.create(question_id: 23, user_id: 4, answer: "I don't know how to get away from Space Boston. Once you are here, you can never leave. It's like Hotel California, only much colder and the people are more like jerks.")
