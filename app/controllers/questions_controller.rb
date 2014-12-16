2bcddeae728d8d58e6e27b0e63bd676b09306cf8
Zoe <doticus@gmail.com>
2014-12-16 15:53:24 -0500

    SPACEOVERFLOW IS BORN!!!!

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -18,14 +18,38 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
     @question.user_id = current_user_id
     if @question.save
-      redirect_to @question, notice: "Question was successfully created"
+      redirect_to @question, :notice => "Question was successfully created"
     else
       render :new
     end
   end
 
+  def edit
+    id = params[:id]
+    @question = Question.find(id)
+  end
+
+  def update
+    id = params[:id]
+    @question = Question.find(id)
+    @question.update(question_params)
+
+      if @question.save
+        redirect_to @question, notice: "Question updated"
+      else
+        render :edit, notice: "Question did not update"
+      end
+  end
+
     def new
       @question = Question.new
+    end
+
+    def destroy
+      id = params[:id]
+      @question = Question.find(id)
+      @question.destroy
+      redirect_to questions_path
     end
 
   def question_params
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
 
 belongs_to :user
-has_many :answers
+has_many :answers, dependent: :destroy
 
 validates_presence_of :user
 
